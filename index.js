1a6e84c3659e7eb8adef402230c1f55518d223dc
Ch3m02 <122635425+Ch3m02@users.noreply.github.com>
2023-03-16 16:54:35 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+document.getElementById("boton_color").addEventListener("click", function () {
+    document.body.style.backgroundColor="#C49BFC"  
+})
+document.getElementById("boton_default").addEventListener("click", function () {
+   document.body.style.backgroundColor="#FFFFFF"
+})
