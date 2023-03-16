8fa2278771016d9bcc5fc183239618745fd33231
Ch3m02 <122635425+Ch3m02@users.noreply.github.com>
2023-03-16 17:33:17 -0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-
-
-document.getElementById("boton_color").addEventListener("click", function () {
-    document.body.style.backgroundColor="#C49BFC"  
-})
-document.getElementById("boton_default").addEventListener("click", function () {
-   document.body.style.backgroundColor="#FFFFFF"
-})
